524dc1cc5d5dc077739beb2f80bbc6614e82812c
Christian Dimas <christian.dimas.94@gmail.com>
2017-09-05 22:30:51 +0700

    finish data konsumen ui

--- source/js/config/routes.jsx
+++ source/js/config/routes.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Login from 'views/Login';
 import Barang from 'views/Barang';
+import Konsumen from 'views/Konsumen';
 import NotFound from 'views/NotFound';
 
 const publicPath = '/';
@@ -10,12 +11,14 @@ const publicPath = '/';
 export const routeCodes = {
   LOGIN: publicPath,
   BARANG: `${ publicPath }barang`,
+  KONSUMEN: `${ publicPath }konsumen`,
 };
 
 export default () => (
   <Switch>
     <Route exact path={ routeCodes.LOGIN } component={ Login } />
     <Route exact path={ routeCodes.BARANG } component={ Barang } />
+    <Route exact path={ routeCodes.KONSUMEN } component={ Konsumen } />
     <Route path='*' component={ NotFound } />
   </Switch>
 );
